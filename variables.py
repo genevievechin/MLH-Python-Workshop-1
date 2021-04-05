90e7cefca92efc8703ba7b400486e8d4d342fae0
Genevieve Chin <>
2021-04-05 08:20:10 +0000

    hello

---
+++ variables.py
@@ -0,0 +1,8 @@
+# A variable with an Integer (whole number) value
+age = 20
+
+# A variable with a String (text) value
+name = "Gen"
+# We can print out the content of variables in a couple of ways:
+print(age)
+      
